f381bf72548943f70d7736c56783e8e37c5a5e9a
ashutosh150195 <ashutosh150195@gmail.com>
2016-09-15 15:23:44 +0530

    Update HighestAmong.java

--- HighestAmong.java
+++ HighestAmong.java
@@ -5,8 +5,7 @@ public class HighestAmong {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int number1 = 1 , number2 = 4, number3 = 3;
-		System.out.println((number1 > number2 && number1 > number3) ? number1 : ((number2 > number1 && number2 > number3) ? number2 : number3));
-
+		System.out.println((number1 >= number2 && number1 >= number3) ? number1 : ((number2 >= number1 && number2 >= number3) ? number2 : number3));
 	}
 
 }
